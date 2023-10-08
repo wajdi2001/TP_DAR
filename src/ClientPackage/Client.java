622e2cdcbf307ca46dd8db3afcbb27d2d50023c5
Wajdi Ben Moumen <wajdibbm@gmail.com>
2023-10-09 00:01:31 +0100

    TP1_DAR

---
+++ src/ClientPackage/Client.java
@@ -0,0 +1,31 @@
+package ClientPackage;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+        System.out.println("je suis un client");
+        Socket s =new Socket("localhost",1234);
+        System.out.println("je suis connecté au serveur");
+        int nb;
+            System.out.println("donner un nombre ");
+        Scanner scanner =new Scanner(System.in);
+        nb =scanner.nextInt();
+
+            OutputStream os =s.getOutputStream();
+
+        InputStream is =s.getInputStream();
+        os.write(nb);
+        int rep = is.read();
+            System.out.println("le produit de "+nb+"*5="+rep);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
---
+++ src/ServerPackage/Server.java
@@ -0,0 +1,30 @@
+package ServerPackage;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("je suis un serveur");
+        try {
+            ServerSocket ss=new ServerSocket(1234);
+            System.out.println("j'attend un client");
+            Socket s=ss.accept();
+            System.out.println("un client est connecté");
+            OutputStream os =s.getOutputStream();
+            InputStream is =s.getInputStream();
+            int nb = is.read();
+            System.out.println("j'ai reçu la valeur "+nb);
+            int rep =nb*5;
+            os.write(rep);
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
