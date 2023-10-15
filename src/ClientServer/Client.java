4359b3efee2fe8844be75de9724ef680a97bf378
wajdi2001 <wajdibbm@gmail.com>
2023-10-15 23:38:17 +0100

    4 modified

---
+++ src/ClientServer/Client.java
@@ -0,0 +1,71 @@
+package ClientServer;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Je suis un client");
+
+            // Établissement d'une connexion avec un serveur local sur le port 1234
+            Socket s = new Socket("localhost", 1234);
+            System.out.println("Je suis connecté au serveur");
+
+            // Déclaration de variables pour les nombres, l'opérateur et le résultat
+            int nb1;
+            int nb2;
+            String op;
+            double res = 0;
+
+            // Crée un objet Scanner pour lire l'entrée utilisateur depuis la console
+            Scanner scanner = new Scanner(System.in);
+
+            // Demande à l'utilisateur de saisir le premier nombre
+            System.out.print("Donnez un nombre1 : ");
+            nb1 = scanner.nextInt();
+
+            // Demande à l'utilisateur de saisir le deuxième nombre
+            System.out.print("Donnez un nombre2 : ");
+            nb2 = scanner.nextInt();
+
+            // Capture la ligne vide après les nombres
+            scanner.nextLine();
+
+            // Demande à l'utilisateur de saisir l'opérateur (+, -, *, /)
+            System.out.print("Donnez un opérateur (+, -, *, /) : ");
+            op = scanner.nextLine();
+
+            // Crée un objet d'opération avec les valeurs saisies
+            Operation op1 = new Operation(nb1, nb2, op);
+
+            // Obtient un flux de sortie à partir de la socket pour envoyer des données
+            OutputStream os = s.getOutputStream();
+
+            // Obtient un flux d'entrée à partir de la socket pour recevoir des données
+            InputStream is = s.getInputStream();
+
+            // Crée des objets pour sérialiser (envoyer) et désérialiser (recevoir) des objets
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+            ObjectInputStream ois = new ObjectInputStream(is);
+
+            // Envoie l'objet d'opération au serveur
+            oos.writeObject(op1);
+
+            // Le serveur renvoie un résultat sous forme de double
+            double result = (Double) ois.readObject();
+
+            // Affiche le résultat à l'utilisateur
+            System.out.println("Résultat : " + result);
+
+            // Ferme la connexion avec le serveur
+            s.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/ClientServer/Server.java
@@ -0,0 +1,56 @@
+package ClientServer;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Je suis un serveur");
+
+        try (ServerSocket serverSocket = new ServerSocket(1234)) {
+            System.out.println("J'attends un client");
+
+            // Accepte une connexion entrante du client
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Un client est connecté");
+
+            try (OutputStream os = clientSocket.getOutputStream(); // Obtient un flux de sortie
+                 InputStream is = clientSocket.getInputStream()) { // Obtient un flux d'entrée
+                ObjectOutputStream oos = new ObjectOutputStream(os);
+                ObjectInputStream ois = new ObjectInputStream(is);
+
+                // Lecture de l'objet d'opération envoyé par le client
+                Operation op1 = (Operation) ois.readObject();
+                int nb1 = op1.getNb1();
+                int nb2 = op1.getNb2();
+                String op = op1.getOp();
+                double res = 0;
+
+                // Effectue l'opération mathématique en fonction de l'opérateur
+                if (op.equals("+")) {
+                    res = nb1 + nb2;
+                } else if (op.equals("-")) {
+                    res = nb1 - nb2;
+                } else if (op.equals("*")) {
+                    res = nb1 * nb2;
+                } else if (op.equals("/")) {
+                    if (nb2 != 0) {
+                        res = (double) nb1 / nb2;
+                    } else {
+                        System.out.println("Division par zéro impossible.");
+                    }
+                }
+
+                // Envoie le résultat au client
+                oos.writeObject(res);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
