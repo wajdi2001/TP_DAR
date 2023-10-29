40fca589637522ef05646b1010fb80828b365d8e
wajdi2001 <wajdibbm@gmail.com>
2023-10-29 20:47:16 +0100

    4 modified

--- src/TP4/Client.java
+++ src/TP4/Client.java
@@ -1,33 +1,48 @@
-package TP4;
+package TP4; // Le code est dans le package TP4
 
 import java.net.DatagramSocket;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
         try {
+            // Crée une nouvelle socket UDP pour le client
             DatagramSocket clientSocket = new DatagramSocket();
-            InetAddress serverAddress = InetAddress.getByName("localhost");
-            int serverPort = 1234;
 
-            String message = "Wajdi Ben Moumen";
-            byte[] sendData = message.getBytes();
+            // Adresse du serveur
+            InetAddress serverAddress = InetAddress.getByName("localhost"); // Ici, "localhost" signifie que le serveur est sur la même machine
+            int serverPort = 1234; // Port du serveur
+
+            // Utilisation d'un scanner pour permettre à l'utilisateur de saisir son prénom
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Donnez votre prénom :\n");
+            String message = scanner.nextLine();
+            byte[] sendData = message.getBytes(); // Convertit la saisie de l'utilisateur en tableau de bytes
+
+            // Crée un paquet contenant les données à envoyer, l'adresse du serveur et le port
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
+
+            // Envoie le paquet au serveur
             clientSocket.send(sendPacket);
 
             // Réception de la réponse du serveur
             byte[] receiveData = new byte[1024];
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            clientSocket.receive(receivePacket);
+            clientSocket.receive(receivePacket); // Attends la réponse du serveur
 
+            // Convertit la réponse en une chaîne de caractères
             String serverResponse = new String(receivePacket.getData(), 0, receivePacket.getLength());
+
+            // Affiche la réponse du serveur ainsi que l'adresse et le port du serveur
             System.out.println("Message du serveur: " + serverResponse);
             System.out.println("Adresse du serveur: " + receivePacket.getAddress() + ", Port du serveur: " + receivePacket.getPort());
 
+            // Ferme la socket du client
             clientSocket.close();
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace(); // En cas d'erreur, affiche l'exception
         }
     }
 }
--- src/TP4/Server.java
+++ src/TP4/Server.java
@@ -7,30 +7,30 @@ import java.net.InetAddress;
 public class Server {
     public static void main(String[] args) {
         try {
-            int serverPort = 1234;
-            DatagramSocket serverSocket = new DatagramSocket(serverPort);
+            int serverPort = 1234; // Définit le port sur lequel le serveur écoute
+            DatagramSocket serverSocket = new DatagramSocket(serverPort); // Crée une socket pour le serveur
 
-            System.out.println("Le serveur est en attente de connexions...");
+            System.out.println("Le serveur est en attente de connexions..."); // Affiche un message indiquant que le serveur est en attente de connexions
 
-            while (true) {
-                byte[] receiveData = new byte[1024];
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                serverSocket.receive(receivePacket);
+            while (true) { // Boucle infinie pour gérer les connexions entrantes
+                byte[] receiveData = new byte[1024]; // Crée un tableau de bytes pour stocker les données reçues
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length); // Crée un paquet pour recevoir des données
+                serverSocket.receive(receivePacket); // Attend la réception de données depuis un client
 
-                String clientMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
-                InetAddress clientAddress = receivePacket.getAddress();
-                int clientPort = receivePacket.getPort();
+                String clientMessage = new String(receivePacket.getData(), 0, receivePacket.getLength()); // Convertit les données reçues en une chaîne de caractères
+                InetAddress clientAddress = receivePacket.getAddress(); // Obtient l'adresse IP du client
+                int clientPort = receivePacket.getPort(); // Obtient le port du client
 
-                System.out.println("Message reçu du client " + clientAddress + ":" + clientPort + ": " + clientMessage);
+                System.out.println("Message reçu du client " + clientAddress + ":" + clientPort + ": " + clientMessage); // Affiche le message reçu du client, ainsi que l'adresse et le port du client
 
                 // Réponse au client
-                String welcomeMessage = "Bienvenu " + clientMessage;
-                byte[] sendData = welcomeMessage.getBytes();
-                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
-                serverSocket.send(sendPacket);
+                String welcomeMessage = "Bienvenu " + clientMessage; // Crée un message de réponse
+                byte[] sendData = welcomeMessage.getBytes(); // Convertit le message de réponse en tableau de bytes
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort); // Crée un paquet pour envoyer la réponse au client
+                serverSocket.send(sendPacket); // Envoie la réponse au client
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace(); // En cas d'erreur, affiche l'exception
         }
     }
 }
