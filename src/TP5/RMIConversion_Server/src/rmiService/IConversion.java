2d294964fd2b88f30a895862ef7a09ea14e5ff09
wajdi2001 <wajdibbm@gmail.com>
2023-11-10 17:18:08 +0100

    4 modified

--- src/TP5/RMIConversion_Server/src/rmiService/IConversion.java
+++
@@ -1,9 +0,0 @@
-package rmiService;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IConversion extends Remote {
-    double convertirMontant(double mt) throws RemoteException;
-
-}
